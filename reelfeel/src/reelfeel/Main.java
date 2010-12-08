4f7b976dc1022f3c67dc1819bf5413f4aad72e4e
valerikirilov <valery@gbg.bg>
2010-12-08 11:34:32 +0200

    this is a show off

---
+++ reelfeel/src/reelfeel/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package reelfeel;
+
+/**
+ *
+ * @author Kirilov
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("eto eto");
+        // TODO code application logic here
+    }
+
+}
